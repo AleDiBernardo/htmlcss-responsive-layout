d1224ae437500b2b1689ab968eee11775e377d1b
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-03-10 17:00:36 +0100

    fix: bug in js script

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const burger_button = document.querySelector('.button');
 const sidebar = document.querySelector('.sidebar');
 const hide_sidebar_btn = document.querySelector('.hide-sidebar');
 const body = document.querySelector('body');
+const navigate = document.querySelector('.navigate')
 
 burger_button.addEventListener('click', function() {
     sidebar.classList.remove('hide');
@@ -16,6 +17,17 @@ hide_sidebar_btn.addEventListener('click', function() {
     sidebar.classList.remove('show');
     sidebar.classList.toggle('hide');
     burger_button.classList.remove('hide');
+    body.classList.remove('overflow-hidden');
+
+});
+
+navigate.addEventListener('click', function() {
+    sidebar.classList.remove('show');
+    sidebar.classList.toggle('hide');
+
+    burger_button.classList.remove('hide');
+    burger_button.classList.toggle('show');
+
     body.classList.remove('overflow-hidden');
 
 });

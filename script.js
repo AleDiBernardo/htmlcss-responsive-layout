2cb3053149875dbfecab159de3e32ac120c03aa2
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-03-10 15:54:47 +0100

    feat: sidebar content and style added

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-const pulsante = document.querySelector('.button');
-const barraLaterale = document.querySelector('.sidebar');
+const burger_button = document.querySelector('.button');
+const sidebar = document.querySelector('.sidebar');
+const hide_sidebar_btn = document.querySelector('.hide-sidebar');
 
-pulsante.addEventListener('click', function() {
-    barraLaterale.classList.toggle('show'); // Aggiunge o rimuove la classe 'show'
+
+burger_button.addEventListener('click', function() {
+    sidebar.classList.remove('hide');
+    sidebar.classList.toggle('show');
+    burger_button.classList.toggle('hide');
+});
+
+hide_sidebar_btn.addEventListener('click', function() {
+    sidebar.classList.remove('show');
+    sidebar.classList.toggle('hide');
+    burger_button.classList.remove('hide');
 });

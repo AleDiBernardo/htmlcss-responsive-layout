162fbfb52275a6edc584270c86538f5913dc46e3
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-03-10 16:21:32 +0100

    fix: fixed scroll when sidebar is open

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 const burger_button = document.querySelector('.button');
 const sidebar = document.querySelector('.sidebar');
 const hide_sidebar_btn = document.querySelector('.hide-sidebar');
-
+const body = document.querySelector('body');
 
 burger_button.addEventListener('click', function() {
     sidebar.classList.remove('hide');
     sidebar.classList.toggle('show');
     burger_button.classList.toggle('hide');
+    body.classList.toggle('overflow-hidden');
+    
+
 });
 
 hide_sidebar_btn.addEventListener('click', function() {
     sidebar.classList.remove('show');
     sidebar.classList.toggle('hide');
     burger_button.classList.remove('hide');
+    body.classList.remove('overflow-hidden');
+
 });
